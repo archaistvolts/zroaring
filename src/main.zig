b41904046110f23a74676d7a2524fd879902b4f1
Travis Staloch <1562827+travisstaloch@users.noreply.github.com>
2026-05-29 16:59:37 -0700

    main: add api-coverage command
    `$ zig build run -- api-coverage --filter roaring_bitmap_`

--- src/main.zig
+++ src/main.zig
@@ -1,55 +1,215 @@
-pub const PositionalArgs = struct {
-    file: []const u8,
-    pattern: []const u8,
+/// cli commands
+const Command = union(enum) {
+    @"api-coverage": struct {
+        @"--filter": []const u8 = "API-COVERAGE-FILTER-NONE",
+    },
+    const Tag = meta.Tag(Command);
 
-    const positionals_fields = std.meta.fieldNames(PositionalArgs);
-    // pub const Mask = std.meta.Int(.unsigned, positionals_fields.len);
-    // const Mask_all_ones = (1 << (positionals_fields.len)) - 1;
-    pub const Error = error{MissingArg};
-    fn usage(exe: []const u8) void {
-        std.debug.print("usage: {s} FILE PATTERN\n", .{exe});
-    }
-    pub fn parse(argiter: *std.process.ArgIterator) Error!PositionalArgs {
-        var seen: usize = 0;
-        var ret: PositionalArgs = undefined;
-        const exe = argiter.next().?;
-        inline for (positionals_fields) |f| {
-            @field(ret, f) = argiter.next() orelse {
-                usage(exe);
-                std.log.err("missing positional arg '{s}'\n", .{f});
-                return error.MissingArg;
-            };
-            seen += 1;
+    pub fn format(c: Command, w: *std.Io.Writer) !void {
+        switch (c) {
+            .@"api-coverage" => |x| {
+                try w.print("api-coverage --filter='{s}'", .{x.@"--filter"});
+            },
         }
-        // std.debug.print("seen {b} all ones {b}\n", .{ seen, Mask_all_ones });
-
-        if (seen != positionals_fields.len) {
-            std.log.err("missing positional arg. expected args to follow: \n", .{});
-            for (positionals_fields[seen..]) |f| std.debug.print("  {s}\n", .{f});
-            usage(exe);
-            return error.MissingArg;
-        }
-        // std.debug.print("seen {b}\n", .{seen});
-        // std.debug.print("parsed args: {s} {s}\n", .{ ret.file, ret.pattern });
-        return ret;
     }
 };
 
-pub fn main() !void {
-    var argiter = std.process.args();
-    const args = try PositionalArgs.parse(&argiter);
-    std.debug.print("file '{s}'\npattern '{s}'\n", .{ args.file, args.pattern });
+fn bail(comptime fmt: []const u8, args: anytype) noreturn {
+    std.debug.print(fmt, args);
+    std.debug.print("Commands: \n", .{});
+    inline for (comptime meta.fieldNames(Command), 0..) |n, i| {
+        if (i != 0) std.debug.print(", ", .{});
+        std.debug.print("{s}: {{\n", .{n});
 
-    // TODO stdin?
-    // var stdin_buf: [4096]u8 = undefined;
-    // var stdin = std.fs.File.stdin().reader(&stdin_buf);
-    // const text = try stdin.interface.takeDelimiter('\n');
-    // const pattern = try stdin.interface.takeDelimiter('\n');
+        const F = @FieldType(Command, n);
+        inline for (comptime meta.fieldNames(F)) |cn| {
+            if (i != 0) std.debug.print(", ", .{});
+            std.debug.print("  {s}: {s}\n", .{ cn, @typeName(@FieldType(F, cn)) });
+        }
+        std.debug.print("}}\n", .{});
+    }
+    std.debug.print("\n", .{});
+    std.process.exit(1);
+}
 
-    // TODO compile and run regex
+pub fn main(init: std.process.Init) !void {
+    var args = init.minimal.args.iterate();
+    _ = args.next();
+    const arg1 = args.next() orelse bail("Missing command argument.\n", .{});
+    var command = if (meta.stringToEnum(Command.Tag, arg1)) |x| switch (x) {
+        inline else => @unionInit(Command, @tagName(x), .{}),
+    } else {
+        bail("Unexpected command argument '{s}'\n", .{arg1});
+    };
 
-    // const r = zroaring.init(args.pattern);
+    // parse args
+    while (args.next()) |arg| {
+        switch (command) {
+            inline else => |_, tag| {
+                const Cf = @FieldType(Command, @tagName(tag));
+                if (meta.stringToEnum(meta.FieldEnum(Cf), arg)) |cmdfield| {
+                    switch (cmdfield) {
+                        inline else => |cmdfieldtag| {
+                            const T = @FieldType(Cf, @tagName(cmdfieldtag));
+                            switch (T) {
+                                []const u8 => @field(
+                                    @field(command, @tagName(tag)),
+                                    @tagName(cmdfieldtag),
+                                ) = args.next() orelse
+                                    bail("Missing command argument: '{s}: {s}'\n", .{ @tagName(cmdfieldtag), @typeName(T) }),
+                                else => @panic("TODO: " ++ @typeName(T)),
+                            }
+                        },
+                    }
+                }
+            },
+        }
+    }
+
+    std.debug.print("\nparsed command:\n  {f}\n", .{command});
+
+    const arena = init.arena.allocator();
+    switch (command) {
+        .@"api-coverage" => {
+            const cr_syms = try collect_api(zroaring.c.root, arena);
+            const bitmap_syms = try collect_api(zroaring.Bitmap, arena);
+            const ctr_syms = try collect_api(zroaring.Container, arena);
+
+            // table from cr_sym to (zr_sym, zr_namespace)
+            var result = std.StringArrayHashMapUnmanaged([2]?[]const u8).empty;
+            // table of found, total for each crprefix
+            var syms_stats_by_prefix: [crprefixes.len][2]f32 = @splat(@splat(0));
+
+            // for each cr_sym search for matching zr_sym and update stats
+            for (cr_syms.keys(), cr_syms.values()) |cr_sym, cr_sym_prefixlen| {
+                const cr_sym_suffix = cr_sym[cr_sym_prefixlen..];
+                const cr_sym_prefix = cr_sym[0..cr_sym_prefixlen];
+                const i = for (crprefixes, 0..) |crprefix, i| {
+                    if (mem.eql(u8, cr_sym_prefix, crprefix)) {
+                        break i;
+                    }
+                } else unreachable;
+                var gop = try result.getOrPut(arena, cr_sym);
+                std.debug.assert(!gop.found_existing);
+                gop.value_ptr.* = .{ null, null };
+                syms_stats_by_prefix[i][1] += 1; // total
+
+                for (bitmap_syms.keys()) |bsym| {
+                    if (mem.eql(u8, cr_sym, bsym) or mem.eql(u8, cr_sym_suffix, bsym)) {
+                        gop.value_ptr.* = .{ bsym, "Bitmap" };
+                        gop.found_existing = true;
+                        syms_stats_by_prefix[i][0] += 1; // found
+                    }
+                }
+
+                for (ctr_syms.keys()) |ctrsym| {
+                    if (!gop.found_existing and
+                        mem.eql(u8, cr_sym, ctrsym) or
+                        mem.eql(u8, cr_sym_suffix, ctrsym))
+                    {
+                        gop.value_ptr.* = .{ ctrsym, "Container" };
+                        syms_stats_by_prefix[i][0] += 1; // found
+                    }
+                }
+            }
+
+            const filter = command.@"api-coverage".@"--filter";
+            var filtered_cr_syms_found: f32 = 0;
+            var filtered_cr_syms_total: f32 = 0;
+            for (cr_syms.keys()) |crsym| {
+                if (mem.containsAtLeast(u8, crsym, 1, filter)) {
+                    const s = result.get(crsym).?;
+                    filtered_cr_syms_total += 1;
+                    if (s[0] != null) {
+                        filtered_cr_syms_found += 1;
+                        std.debug.print("{s: <50} {?s}.{?s}\n", .{ crsym, s[1], s[0] });
+                    } else std.debug.print("{s: <50} --\n", .{crsym});
+                }
+            }
+
+            std.debug.print("\nsymbols coverage:\n  {s: <20}: {s: <20}:\n", .{ "prefix", "found / total / %" });
+            const sep = "---------------------------------------------\n";
+            std.debug.print(sep, .{});
+            for (crprefixes, syms_stats_by_prefix) |crp, stat| {
+                const found, const total = stat;
+                std.debug.print(
+                    "  {s: <20}: {: <5} / {: <5} / {:2.1}%\n",
+                    .{ crp, found, total, found / total * 100 },
+                );
+            }
+            {
+                var totals: [2]f32 = @splat(0);
+                for (syms_stats_by_prefix) |stat| {
+                    totals[0] += stat[0];
+                    totals[1] += stat[1];
+                }
+                const found, const total = totals;
+                std.debug.print(sep, .{});
+                std.debug.print(
+                    "  {s: <20}: {: <5} / {: <5} / {:2.1}%\n",
+                    .{ "total", found, total, found / total * 100 },
+                );
+            }
+            {
+                const total = filtered_cr_syms_total;
+                const found = filtered_cr_syms_found;
+                std.debug.print(sep, .{});
+                std.debug.print(
+                    "  {s: <20}: {: <5} / {: <5} / {:2.1}%\n",
+                    .{ "filtered", found, total, found / total * 100 },
+                );
+            }
+        },
+    }
+}
+
+const crprefixes = [_][]const u8{
+    "roaring_bitmap_",
+    "ra_",
+    "container_",
+    "run_container_",
+    "bitset_container_",
+    "array_container_",
+};
+
+/// map of found names to prefix len after which match is found
+const StringMap = std.StringArrayHashMapUnmanaged(usize);
+
+fn collect_api(T: type, arena: mem.Allocator) !StringMap {
+    var r = StringMap.empty;
+    try r.ensureTotalCapacity(arena, 10_000);
+    @setEvalBranchQuota(30_000);
+    const is_croaring = T == zroaring.c.root;
+    for (meta.declarations(T)) |decl| {
+        const declname = decl.name;
+        if (!is_croaring or
+            for (crprefixes) |crprefix| {
+                if (mem.startsWith(u8, declname, crprefix)) break true;
+            } else false)
+        {
+            const l: usize = if (!is_croaring)
+                0
+            else for (crprefixes) |p| {
+                if (mem.startsWith(u8, declname, p)) break p.len;
+            } else unreachable;
+
+            if (!mem.endsWith(u8, declname, "_s") and
+                !mem.endsWith(u8, declname, "_t"))
+                r.putAssumeCapacity(declname, l);
+        }
+    }
+    const C = struct {
+        keys: [][]const u8,
+        pub fn lessThan(c: @This(), a: usize, b: usize) bool {
+            return mem.order(u8, c.keys[a], c.keys[b]) == .lt;
+        }
+    };
+    r.sortUnstable(C{ .keys = r.keys() });
+    return r;
 }
 
 const std = @import("std");
+const mem = std.mem;
+const meta = std.meta;
+const Type = std.builtin.Type;
 const zroaring = @import("zroaring");
